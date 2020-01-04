56184f2c7417caaec402767affc23752ce00ce8c
ThuongTC <truongcongthuongz@gmail.com>
2020-01-03 20:47:03 -0800

    reset source

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { ShowNotiReducers } from './sidebarReducers';
-import { IntlReducer as Intl } from 'react-redux-multilingual';
-
-const reducers = combineReducers({
-    ShowNotiReducers,
-    Intl,
-});
-export default reducers;
--- src/redux/type.js
+++
@@ -1,5 +0,0 @@
-// Window action type
-export const OPEN_SIDEBAR = "OPEN_SIDEBAR";
-export const OPEN_USER_BAR = "OPEN_USER_BAR";
-
-export const LOAD_INIT_DATA = "LOAD_INIT_DATA";
